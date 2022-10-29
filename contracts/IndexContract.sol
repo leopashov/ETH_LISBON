459aa97b518c26ee5be7bf832321f9c6f606b131
leo <lp916@ic.ac.uk>
2022-10-29 14:56:07 +0200

    calculated vault token priced in underlying - now need chainlink

--- contracts/IndexContract.sol
+++ contracts/IndexContract.sol
@@ -1,34 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface IIndexToken {
+// import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Burnable.sol";
+
+interface IIndexToken is IERC20 {
     // interface to interact with token contract
     function mint(address to, uint256 amount) external;
+}
 
-    function allowance(address owner, address spender)
-        external
-        view
-        returns (uint256);
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) external returns (bool);
-
-    // function burn(uint256 amount) external virtual {
-    //     _burn(_msgSender(), amount);
-    // }
-
-    function totalSupply() external view returns (uint256);
+interface IYearnVaultToken is IERC20 {
+    function pricePerShare() external view returns (uint256);
 }
 
 contract IndexContract {
     // Define 'global' variables
     IIndexToken public tokenContract;
+
     address[] private _tokens;
+    address[] private _vaultTokens;
     uint256 public poolValue; // pool value quoted in eth
     uint256 public currentTokenSupply;
     mapping(address => uint256) public tokenIndexValues; // maps token address to value (in eth) of that token in the index
@@ -115,22 +106,32 @@ contract IndexContract {
         payable(msg.sender).transfer(amount); //typecast 'payable' to msg.sender
     }
 
-    // function getIndexBalance() public {
-    //     // gets current balance of index tokens
-    //     for (uint8 i = 0; i < _tokens.length; i++) {
-    //         address token = _tokens[i];
-    //         //calculate value of token in vault
-    //         // uint256 tokenVaultValue = calculateTokenVaultValue(token);
-    //         // tokenIndexValues[token] = tokenVaultValue;
-    //     }
-    // }
+    function getIndexBalance() public {
+        // gets current balance of index tokens
+        for (uint8 i = 0; i < _vaultTokens.length; i++) {
+            address token = _vaultTokens[i];
+            //calculate value of token in vault
+            uint256 tokenVaultValue = calculateTokenVaultValue(token);
+            tokenIndexValues[token] = tokenVaultValue;
+        }
+    }
 
-    // function calculateTokenVaultValue(address token) {
-    //     uint256 vaultTokensHeld = IERC20(token).balanceOf(address(this));
-    // }
+    function calculateTokenVaultValue(address vaultToken) public {
+        uint256 numberOfVaultTokensHeld = IERC20(vaultToken).balanceOf(
+            address(this)
+        );
+        uint256 individualVaultTokenValue = calculateVaultTokenPrice();
+        return (numberOfVaultTokensHeld * individualVaultTokenValue);
+    }
+
+    function calculateVaultTokenPrice(address vaultToken) public {
+        // get price of vault token quoted in underlying
+        uint256 vaultTokenPriceInUnderlying = IYearnVaultToken(vaultToken)
+            .pricePerShare();
+        // ### get price of underlying in eth => CHAINLINK REQUIRED ###
+    }
 
     // function swapEthForToken() {}
-
     // // swap eth for token depending on constant balancing of the pools
 
     // function balanceFund() {
