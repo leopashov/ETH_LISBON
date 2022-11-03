3b064bebfb6007a614acb52d103f018b811e192e
leo <lp916@ic.ac.uk>
2022-11-03 10:21:13 +0100

    connected to goerli and tested. WIll allow integration testing

--- tests/aaveConnectionTest.ts
+++ tests/aaveConnectionTest.ts
@@ -1,33 +1,34 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { Signer } from "ethers";
+import { Contract, Signer } from "ethers";
 import { ethers } from "hardhat";
 import { PromiseOrValue } from "../typechain-types/common";
-import * as WethABI from "./WethABI.json";
+import { abi as WethABI } from "../artifacts/contracts/IndexToken.sol/IndexToken.json"; //indexToken is ERC20
 
-describe("aave connection", async() => {
+describe("Goerli connection", async() => {
 
 
     let accounts: SignerWithAddress[];
+    let WethContract: Contract;
 
     beforeEach(async() => {
         // const provider = ethers.getDefaultProvider("goerli", options)
         accounts = await ethers.getSigners();
         const signer = accounts[0];
         const goerliWethTokenAddress = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6';
-        const WethContract = new ethers.Contract(goerliWethTokenAddress, WethABI, signer);
+        console.log(WethABI, typeof(WethABI));
+        WethContract = new ethers.Contract(goerliWethTokenAddress, WethABI, signer);
     })
 
     describe("some description lol", async() => { 
-        // const goerliWethTokenAddress = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6';
-        // const WethContract = new ethers.Contract(goerliWethTokenAddress, WethABI, signer);
 
         it("should get the latest goerli block", async() => {
             
-            Signer.getBlock();
+            const latestBlock = await ethers.provider.getBlock("latest");
+            expect(latestBlock.number).to.be.above(7883340); // block number at time of writing test
         })
         
-        it("should be able to weth data", async() => {
+        it("should be able to call weth data", async() => {
             const callInfo = await WethContract.decimals()
             console.log(callInfo);
             expect(callInfo).to.eq(18);
