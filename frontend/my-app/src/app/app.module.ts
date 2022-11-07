e0e4c520959709ec38ea638e2c44561f0ab0dd14
Jannik Hehemann <jannik@hehemann-melle.de>
2022-11-07 21:41:27 +0100

    updated dashboard site and created home and swap

--- frontend/my-app/src/app/app.module.ts
+++ frontend/my-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SwapComponent } from './swap/swap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    SwapComponent
   ],
   imports: [
     BrowserModule,
